d74fb435dba68d50b6a4d4296c68447dcf004ac6
Zhivko Georgiev <zdgeorgiev@abv.bg>
2014-06-09 23:45:23 +0300

    updated file structure

--- C# Basics Homeworks/1. Introduction-to-Programming-Homework/05. PrintMyName/PrintMyName.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-class PrintMyName
-{
-    static void Main()
-    {
-        Console.WriteLine("Hello! My name is Zhivko :) Nice to \"meet\" you!");
-    }
-}
--- C# Basics Homeworks/1. Introduction-to-Programming-Homework/14. DateAndTime/DateAndTime.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-class DateAndTime
-{
-    static void Main()
-    {
-        Console.WriteLine("The current date and time is : {0}", DateTime.Now);
-    }
-}
