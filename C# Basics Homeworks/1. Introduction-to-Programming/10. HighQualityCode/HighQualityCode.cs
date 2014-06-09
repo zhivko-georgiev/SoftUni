cd2f8e7e35f189ab0b68932f8894befe6a58baff
Zhivko Georgiev <zdgeorgiev@abv.bg>
2014-06-09 23:42:20 +0300

    updated solution structure

---
+++ C# Basics Homeworks/1. Introduction-to-Programming/10. HighQualityCode/HighQualityCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class HighQualityCode
+{
+    static void Main()
+    {
+        Console.WriteLine("Hi, I am NOT horribly formatted program");
+        Console.WriteLine("Numbers and squares:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i + " --> " + i * i);
+        }
+    }
+}
---
+++ C# Basics Homeworks/1. Introduction-to-Programming/14. DateAndTime/DateAndTime.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class DateAndTime
+{
+    static void Main()
+    {
+        Console.WriteLine("The current date and time is : {0}", DateTime.Now);
+    }
+}
